a35185a8b7631867438995c8c158c8b92c47cc02
yohan <noyohanx@naver.com>
2023-03-09 16:15:54 +0900

    :sparkles: party get api 생성

---
+++ api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import *
+
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Party
+        fields = ('*')
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .serializers import *
+from .models import Party
 # Create your views here.
+class PartyList(APIView):
+    def get(self, request):
+        queryset = Party.objects.all()
+        serializer = PartySerializer(queryset, many=True)
+
+        return Response(serializer.data)
