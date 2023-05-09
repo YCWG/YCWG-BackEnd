fb92f4eafedbe11cb6dbfe83c93d82ee1e5e732d
yohan <noyohanx@naver.com>
2023-05-09 15:58:01 +0900

    :bug: :: max_length 제거

--- party_list/models.py
+++ party_list/models.py
@@ -4,6 +4,6 @@ class Party(models.Model):
     title = models.CharField(max_length=255)
     category = models.CharField(max_length=24)
     place = models.CharField(max_length=255)
-    member_limit = models.IntegerField(max_length=50)
+    member_limit = models.IntegerField()
     date = models.DateTimeField()
     description = models.TextField(blank=True)
