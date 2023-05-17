73d4b29d2963fc4a49fa34c3b794d318cfa6fd5a
yohan <noyohanx@naver.com>
2023-05-17 20:09:11 +0900

    :sparkles: :: 로그인 관련 기능 추가

--- ycwg/urls.py
+++ ycwg/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('partylist/', include("party_list.urls")),
     path('admin/', admin.site.urls),
     path('api/accounts/v1/', include('accounts.urls')),
+    path('auth/', include('auth.urls'))
 ]
