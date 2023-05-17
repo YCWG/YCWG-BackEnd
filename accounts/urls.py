4133f63898190cfe104fea0c278db0ddb07fd0de
yohan <noyohanx@naver.com>
2023-05-18 08:52:22 +0900

    :fire: :: delete accounts 파일

--- accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('registration/', include('dj_rest_auth.registration.urls')),
-    path('', include('dj_rest_auth.urls'))
-]
--- auth/views.py
+++ auth/views.py
@@ -1,7 +1,14 @@
-from .serailizers import MyTokenObtainPairSerializer
+from django.contrib.auth.models import User
+from .serailizers import MyTokenObtainPairSerializer, RegisterSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
