f4f950f46f96e167e19b568660246433c53501d5
yohan <noyohanx@naver.com>
2023-05-22 13:59:34 +0900

    login view 완성

--- account/views.py
+++ account/views.py
@@ -1,42 +1,53 @@
-import json
+from django.contrib.auth import authenticate
+from django.conf import settings
+from django.middleware import csrf
+from rest_framework import exceptions as rest_exceptiojns, response, decorators as rest_decoratros, permission as rest_permission
+from rest_framework_simplejwt import tokens, views as jwt_views, serializers as jwt_serailizers, exceptions as rest_exceptions
+from account import serializers, models
 
-from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
-from django.middleware.csrf import get_token
-from django.views.decorators.http import require_POST
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import SessionAuthentication
+def get_user_token(user):
+    refresh = tokens.RefreshToken.for_user(user)
+    return(
+        "refresh_token" : str(refresh),
+        "access_token" : str(refresh.access_token)
+    )
 
-def get_csrf(request):
-    response = JsonResponse({"Info" : "Success - Set CSRF Cookie"})
-    response["X-CSRFToken"] = get_token(request)
-    return request
-
-@require_POST
+@rest_decoratros.api_view("POST")
+@rest_decoratros.permission_classes([])
 def loginView(request):
-    data = json.loads(request.body)
-    username = data.get("username")
-    password = data.get("password")
+    serializer = serializers.LoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception = True)
 
-    if username is None or password is None:
-        return JsonResponse({"info":"Username and Passwrod is needed"})
-    
-    user = authenticate(username = username, password = password)
+    email = serializer.validated_data["email"]
+    password = serializer.validated_data["password"]
 
-    if user is None:
-        return JsonResponse({"info":"User does not exist"}, status = 400)
-    
-    login(request, user)
-    return JsonResponse({"info":"User logged in successfully"})
+    user = authenticate(email=email, password=password)
 
-class WhoAmIView(APIView):
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [IsAuthenticated]
+    if user is not None:
+        tokens = get_user_token(user)
+        res = response.Response()
+        res.set_cookie(
+            key = settings.SIMPLE_JWT['AUTH_COOKIE'],
+            value = tokens["access_token"],
+            expires= settings.SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'],
+            secure= settings.SIMPLE_JWTp['AUTH_COOKIE_SECURE'],
+            httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
 
-    @staticmethod
-    def get(request, format = None):
-        print(request.user.username)
-        return JsonResponse({"username", request.user.username})
-    
-    
+        )
+
+        res.set_cookie(
+            key = settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'],
+            value = tokens["refresh_token"],
+            expires= settings.SIMPLE_JWT['REFRESH_TOKEN_LIFETIME'],
+            secure= settings.SIMPLE_JWTp['AUTH_COOKIE_SECURE'],
+            httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
+
+        )
+
+        res.data = tokens
+        res["X-CSRFToken"] = csrf.get_token(request)
+        return res
+    raise rest_exceptions.AuthenticationFailed(
+        "Email or Password is incorrect")
