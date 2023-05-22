170104924979e96ff08b92bdb75fa331380d862a
yohan <noyohanx@naver.com>
2023-05-22 12:20:56 +0900

    account 모델 완성

--- account/models.py
+++ account/models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from django.db import models 
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
@@ -33,3 +34,27 @@ class AccountManager(BaseUserManager):
         user.is_superuser = True
         user.save(using=self._db)
         return
+    
+class Account(AbstractBaseUser):
+    email = models.EmailField(null = False, blank = False, unique = True),
+    username = models.CharField(max_length = 50, blank = False, null = False)
+    is_admin = models.BooleanField(default = False)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    is_superuser = models.BooleanField(default = False)
+    create_dt = models.DateTimeField(auto_now_add = True)
+    updated_dt = models.DateTimeField(auto_now = True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
+    
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label):
+        return True
