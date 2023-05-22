b42e968698364f7760af12f746ae5be67acdb723
yohan <noyohanx@naver.com>
2023-05-22 12:13:58 +0900

    django contrib auth model을 이용하여 모델 설계

---
+++ account/models.py
@@ -0,0 +1,35 @@
+from django.db import models 
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+class AccountManager(BaseUserManager):
+
+    def create_user(self, email, username, password=None, **kwargs):
+        
+        if not email:
+            raise ValueError("Email is required")
+        
+        if not username:
+            raise ValueError("Username is required")
+    
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, email, username, password, **kwargs):
+        user = self.create_user(
+            email = self.normalize_email(email),
+            username = username,
+            password = password
+        )
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return
