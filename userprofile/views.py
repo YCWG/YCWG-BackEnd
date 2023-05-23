cac31dda0b5db27d1d3170cd93647a8b80569bbf
yohan <noyohanx@naver.com>
2023-05-23 16:47:58 +0900

    user profile 기능 1차 완성(수정 해야할?듯?)

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from .serializers import UserProfileSerializer
+from account import models
 
-# Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = UserProfileSerializer
+    queryset = models.Account.objects.all()
+
--- ycwg/urls.py
+++ ycwg/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
-from rest_framework.routers import DefaultRouter
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('partylist/', include("party_list.urls")),
     path('admin/', admin.site.urls),
-    path('api/auth/', include('account.urls'))
+    path('api/auth/', include('account.urls')),
+    path('profile/', include('userprofile.urls')),
 ]
