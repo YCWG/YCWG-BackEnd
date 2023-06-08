d9c9828c3ee62fc3a4bdbfd70a508a4ef6dc5657
yohan <noyohanx@naver.com>
2023-06-09 08:50:37 +0900

    :recycle: :: 모델 수정

--- party_list/models.py
+++ party_list/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Party(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="")
+    image = models.ImageField(upload_to='', blank=True, null=True)
     category = models.CharField(max_length=24)
     place = models.CharField(max_length=255)
     member_limit = models.IntegerField()
@@ -12,3 +12,10 @@ class Party(models.Model):
     description = models.TextField(blank=True)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    upload_date = models.DateTimeField('UPLOAD DATE', auto_now_add=True)
+
+    class Meta:
+        ordering = ('upload_date',)
+
+    def __str__(self):
+        return self.title
