62bb965f761a374cc604f71c4ee37c9090b7b321
yohan <noyohanx@naver.com>
2023-06-09 07:51:56 +0900

    :bug: :: 문자열 에러 수정

--- party_list/serializers.py
+++ party_list/serializers.py
@@ -4,7 +4,7 @@ from .models import *
 class PartyListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
-        fields = 'title, category, place, member_limit, now_member, date, description, latitude, longitude'
+        fields = 'title', 'category', 'place', 'member_limit', 'now_member', 'date', 'description', 'latitude', 'longitude'
 
 class PartyImageSerializer(serializers.ModelSerializer):
     class Meta:
