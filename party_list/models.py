7b13b4ad57d7a141b07a8df84be1c50361bee681
yohan <noyohanx@naver.com>
2023-06-08 23:24:12 +0900

    :recycle: :: 모델 변경

--- party_list/models.py
+++ party_list/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Party(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="uploads/")
+    image = models.CharField(max_length=255)
     category = models.CharField(max_length=24)
     place = models.CharField(max_length=255)
     member_limit = models.IntegerField()
