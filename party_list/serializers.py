600f648c8f178ac9d93c8909dab740eaa401c592
yohan <noyohanx@naver.com>
2023-06-09 08:39:47 +0900

    :recycle: :: 수정

--- party_list/serializers.py
+++ party_list/serializers.py
@@ -4,9 +4,9 @@ from .models import *
 class PartyListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
-        fields = ('title', 'category', 'place', 'member_limit', 'now_member', 'date', 'description', 'latitude', 'longitude',)
+        fields = ('id','title', 'category', 'place', 'member_limit', 'now_member', 'date', 'description', 'latitude', 'longitude',)
 
 class PartyImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Party
-        fields = 'image'
+        fields = ('image')
