3fff5d9854c2aad37db472f4b14cc3220281bdc4
yohan <noyohanx@naver.com>
2023-06-09 07:46:39 +0900

    :bug: :: url 추가

--- party_list/urls.py
+++ party_list/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PartyListAPIView, PartyListDetailAPIView
+from .views import PartyListAPIView, PartyListDetailAPIView, PartyImageAPIView
 
 urlpatterns = [
     path('', PartyListAPIView.as_view(), name = "partylist"),
-    path("<int:pk>/", PartyListDetailAPIView.as_view(), name = "partylist_detail")
+    path("<int:pk>/", PartyListDetailAPIView.as_view(), name = "partylist_detail"),
+    path("image/", PartyImageAPIView.as_view(), name = "partyimage")
 ]
